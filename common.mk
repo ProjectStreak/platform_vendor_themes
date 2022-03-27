01020ca43c2290a0774e664d8bd7d1e93e5e2b69
00day0 <therandomuser11@gmail.com>
2022-03-27 22:11:06 +0200

    themes: Import our themed icons
    @ejbtrd:
     - Build it only on vanilla
    
    Change-Id: I791d835f89fda52bc5bd3cf5a94054b8d2489bab

--- common.mk
+++ common.mk
@@ -6,3 +6,8 @@ include vendor/themes/iconpacks/iconpacks.mk
 
 # Icon Shapes
 include vendor/themes/iconshapes/iconshapes.mk
+
+# Themed Icons
+ifneq ($(TARGET_BUILD_GAPPS),true)
+PRODUCT_PACKAGE_OVERLAYS += vendor/themes/themed_icons
+endif
